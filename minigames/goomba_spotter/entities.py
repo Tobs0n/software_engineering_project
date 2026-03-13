3b525c91b9721625c6387f0acbf8129799828714
RLK-UvA <ruben.louwe.kooijmans@uva.student.nl>
2026-03-13 16:21:17 +0100

    Hud class toegevoegd aan goomba_spotter

--- minigames/goomba_spotter/entities.py
+++ minigames/goomba_spotter/entities.py
@@ -9,6 +9,9 @@ BUZZY_BEETLE = UnitType(color=(50, 50, 180), radius=15)
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self, speed, position, color, size=20):
+        """
+        Initialize with a starting position, move speed, size and color
+        """
         super().__init__()
         self.speed = speed
         self.pos = position
@@ -19,8 +22,14 @@ class Entity(pygame.sprite.Sprite):
         self.pos += self.speed
 
     def is_offscreen(self, width):
+        """
+        Calculate if the entity is off the screen
+        """
         return self.pos.x > width + self.radius
 
     def draw(self, surface):
+        """
+        display the entity
+        """
         pygame.draw.circle(surface, self.color, (int(self.pos.x), int(self.pos.y)), self.radius)
 
