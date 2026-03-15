f7d3126692847ff83c31463686927b2b806e6f5b
jiri132 <jiri28beurskens@gmail.com>
2026-03-15 16:19:03 +0100

    Overhaul incomming!

--- src/spaces/lobby/lobbyState.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class LobbyState(enum.Enum):
-    WAITING_FOR_PLAYERS = 1
-    READY_TO_START = 2
-    IN_GAME = 3
-    GAME_ENDED = 4
--- src/spaces/lobby/playlistMode.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class PlaylistMode(Enum):
-    SEQUENTIAL = 1
-    RANDOM = 2
